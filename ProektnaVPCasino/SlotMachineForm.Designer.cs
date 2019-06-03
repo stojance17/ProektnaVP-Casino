81f76abf07274e906d429ca5882a8c0b8a214920
Marko <marsy.krstevski@gmail.com>
2019-06-04 00:06:12 +0200

    Added SlotMachine Game

---
+++ ProektnaVPCasino/SlotMachineForm.Designer.cs
@@ -0,0 +1,392 @@
+﻿namespace ProektnaVPCasino
+{
+    partial class SlotMachineForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Slot1 = new System.Windows.Forms.PictureBox();
+            this.Slot2 = new System.Windows.Forms.PictureBox();
+            this.Slot3 = new System.Windows.Forms.PictureBox();
+            this.Slot4 = new System.Windows.Forms.PictureBox();
+            this.Slot5 = new System.Windows.Forms.PictureBox();
+            this.Slot6 = new System.Windows.Forms.PictureBox();
+            this.Slot7 = new System.Windows.Forms.PictureBox();
+            this.Slot8 = new System.Windows.Forms.PictureBox();
+            this.Slot9 = new System.Windows.Forms.PictureBox();
+            this.Slot10 = new System.Windows.Forms.PictureBox();
+            this.Slot11 = new System.Windows.Forms.PictureBox();
+            this.Slot12 = new System.Windows.Forms.PictureBox();
+            this.Slot13 = new System.Windows.Forms.PictureBox();
+            this.Slot14 = new System.Windows.Forms.PictureBox();
+            this.Slot15 = new System.Windows.Forms.PictureBox();
+            this.totalMoney = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Bet = new System.Windows.Forms.NumericUpDown();
+            this.SpinBtn = new System.Windows.Forms.Button();
+            this.CashOutBtn = new System.Windows.Forms.Button();
+            this.WinLabel = new System.Windows.Forms.Label();
+            this.WinTB = new System.Windows.Forms.TextBox();
+            this.musicBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Slot1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Slot2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Slot3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Slot4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Slot5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Slot6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Slot7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Slot8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Slot9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Slot10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Slot11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Slot12)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Slot13)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Slot14)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Slot15)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Bet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Slot1
+            // 
+            this.Slot1.Location = new System.Drawing.Point(12, 30);
+            this.Slot1.Name = "Slot1";
+            this.Slot1.Size = new System.Drawing.Size(126, 85);
+            this.Slot1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Slot1.TabIndex = 0;
+            this.Slot1.TabStop = false;
+            // 
+            // Slot2
+            // 
+            this.Slot2.Location = new System.Drawing.Point(144, 30);
+            this.Slot2.Name = "Slot2";
+            this.Slot2.Size = new System.Drawing.Size(126, 85);
+            this.Slot2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Slot2.TabIndex = 1;
+            this.Slot2.TabStop = false;
+            // 
+            // Slot3
+            // 
+            this.Slot3.Location = new System.Drawing.Point(276, 30);
+            this.Slot3.Name = "Slot3";
+            this.Slot3.Size = new System.Drawing.Size(126, 85);
+            this.Slot3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Slot3.TabIndex = 2;
+            this.Slot3.TabStop = false;
+            // 
+            // Slot4
+            // 
+            this.Slot4.Location = new System.Drawing.Point(408, 30);
+            this.Slot4.Name = "Slot4";
+            this.Slot4.Size = new System.Drawing.Size(126, 85);
+            this.Slot4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Slot4.TabIndex = 3;
+            this.Slot4.TabStop = false;
+            // 
+            // Slot5
+            // 
+            this.Slot5.Location = new System.Drawing.Point(540, 30);
+            this.Slot5.Name = "Slot5";
+            this.Slot5.Size = new System.Drawing.Size(126, 85);
+            this.Slot5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Slot5.TabIndex = 4;
+            this.Slot5.TabStop = false;
+            // 
+            // Slot6
+            // 
+            this.Slot6.Location = new System.Drawing.Point(12, 121);
+            this.Slot6.Name = "Slot6";
+            this.Slot6.Size = new System.Drawing.Size(126, 85);
+            this.Slot6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Slot6.TabIndex = 5;
+            this.Slot6.TabStop = false;
+            // 
+            // Slot7
+            // 
+            this.Slot7.Location = new System.Drawing.Point(144, 121);
+            this.Slot7.Name = "Slot7";
+            this.Slot7.Size = new System.Drawing.Size(126, 85);
+            this.Slot7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Slot7.TabIndex = 6;
+            this.Slot7.TabStop = false;
+            // 
+            // Slot8
+            // 
+            this.Slot8.Location = new System.Drawing.Point(276, 121);
+            this.Slot8.Name = "Slot8";
+            this.Slot8.Size = new System.Drawing.Size(126, 85);
+            this.Slot8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Slot8.TabIndex = 7;
+            this.Slot8.TabStop = false;
+            // 
+            // Slot9
+            // 
+            this.Slot9.Location = new System.Drawing.Point(408, 121);
+            this.Slot9.Name = "Slot9";
+            this.Slot9.Size = new System.Drawing.Size(126, 85);
+            this.Slot9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Slot9.TabIndex = 8;
+            this.Slot9.TabStop = false;
+            // 
+            // Slot10
+            // 
+            this.Slot10.Location = new System.Drawing.Point(540, 121);
+            this.Slot10.Name = "Slot10";
+            this.Slot10.Size = new System.Drawing.Size(126, 85);
+            this.Slot10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Slot10.TabIndex = 9;
+            this.Slot10.TabStop = false;
+            // 
+            // Slot11
+            // 
+            this.Slot11.Location = new System.Drawing.Point(12, 212);
+            this.Slot11.Name = "Slot11";
+            this.Slot11.Size = new System.Drawing.Size(126, 85);
+            this.Slot11.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Slot11.TabIndex = 10;
+            this.Slot11.TabStop = false;
+            // 
+            // Slot12
+            // 
+            this.Slot12.Location = new System.Drawing.Point(144, 212);
+            this.Slot12.Name = "Slot12";
+            this.Slot12.Size = new System.Drawing.Size(126, 85);
+            this.Slot12.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Slot12.TabIndex = 11;
+            this.Slot12.TabStop = false;
+            // 
+            // Slot13
+            // 
+            this.Slot13.Location = new System.Drawing.Point(276, 212);
+            this.Slot13.Name = "Slot13";
+            this.Slot13.Size = new System.Drawing.Size(126, 85);
+            this.Slot13.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Slot13.TabIndex = 12;
+            this.Slot13.TabStop = false;
+            // 
+            // Slot14
+            // 
+            this.Slot14.Location = new System.Drawing.Point(408, 212);
+            this.Slot14.Name = "Slot14";
+            this.Slot14.Size = new System.Drawing.Size(126, 85);
+            this.Slot14.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Slot14.TabIndex = 13;
+            this.Slot14.TabStop = false;
+            // 
+            // Slot15
+            // 
+            this.Slot15.Location = new System.Drawing.Point(540, 212);
+            this.Slot15.Name = "Slot15";
+            this.Slot15.Size = new System.Drawing.Size(126, 85);
+            this.Slot15.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Slot15.TabIndex = 14;
+            this.Slot15.TabStop = false;
+            // 
+            // totalMoney
+            // 
+            this.totalMoney.Enabled = false;
+            this.totalMoney.Location = new System.Drawing.Point(84, 502);
+            this.totalMoney.Name = "totalMoney";
+            this.totalMoney.Size = new System.Drawing.Size(100, 20);
+            this.totalMoney.TabIndex = 20;
+            this.totalMoney.Text = "10000";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 505);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "Total";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(230, 505);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "Bet:";
+            // 
+            // Bet
+            // 
+            this.Bet.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.Bet.Location = new System.Drawing.Point(292, 502);
+            this.Bet.Maximum = new decimal(new int[] {
+            300,
+            0,
+            0,
+            0});
+            this.Bet.Name = "Bet";
+            this.Bet.ReadOnly = true;
+            this.Bet.Size = new System.Drawing.Size(120, 20);
+            this.Bet.TabIndex = 23;
+            this.Bet.TabStop = false;
+            this.Bet.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // SpinBtn
+            // 
+            this.SpinBtn.Location = new System.Drawing.Point(536, 500);
+            this.SpinBtn.Name = "SpinBtn";
+            this.SpinBtn.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.SpinBtn.Size = new System.Drawing.Size(75, 23);
+            this.SpinBtn.TabIndex = 24;
+            this.SpinBtn.Text = "Spin";
+            this.SpinBtn.UseVisualStyleBackColor = true;
+            this.SpinBtn.Click += new System.EventHandler(this.SpinBtn_Click);
+            this.SpinBtn.KeyDown += new System.Windows.Forms.KeyEventHandler(this.SpinBtn_KeyDown);
+            // 
+            // CashOutBtn
+            // 
+            this.CashOutBtn.Location = new System.Drawing.Point(686, 499);
+            this.CashOutBtn.Name = "CashOutBtn";
+            this.CashOutBtn.Size = new System.Drawing.Size(75, 23);
+            this.CashOutBtn.TabIndex = 25;
+            this.CashOutBtn.TabStop = false;
+            this.CashOutBtn.Text = "Cash Out";
+            this.CashOutBtn.UseVisualStyleBackColor = true;
+            this.CashOutBtn.Click += new System.EventHandler(this.CashOutBtn_Click);
+            // 
+            // WinLabel
+            // 
+            this.WinLabel.AutoSize = true;
+            this.WinLabel.Location = new System.Drawing.Point(24, 399);
+            this.WinLabel.Name = "WinLabel";
+            this.WinLabel.Size = new System.Drawing.Size(50, 13);
+            this.WinLabel.TabIndex = 26;
+            this.WinLabel.Text = "TotalWin";
+            // 
+            // WinTB
+            // 
+            this.WinTB.Enabled = false;
+            this.WinTB.Location = new System.Drawing.Point(84, 396);
+            this.WinTB.Name = "WinTB";
+            this.WinTB.Size = new System.Drawing.Size(100, 20);
+            this.WinTB.TabIndex = 27;
+            this.WinTB.Text = "0";
+            // 
+            // musicBtn
+            // 
+            this.musicBtn.Location = new System.Drawing.Point(686, 386);
+            this.musicBtn.Name = "musicBtn";
+            this.musicBtn.Size = new System.Drawing.Size(75, 75);
+            this.musicBtn.TabIndex = 28;
+            this.musicBtn.TabStop = false;
+            this.musicBtn.Text = "Music";
+            this.musicBtn.UseVisualStyleBackColor = true;
+            this.musicBtn.Click += new System.EventHandler(this.musicBtn_Click);
+            // 
+            // SlotMachineForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 537);
+            this.Controls.Add(this.musicBtn);
+            this.Controls.Add(this.WinTB);
+            this.Controls.Add(this.WinLabel);
+            this.Controls.Add(this.CashOutBtn);
+            this.Controls.Add(this.SpinBtn);
+            this.Controls.Add(this.Bet);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.totalMoney);
+            this.Controls.Add(this.Slot15);
+            this.Controls.Add(this.Slot14);
+            this.Controls.Add(this.Slot13);
+            this.Controls.Add(this.Slot12);
+            this.Controls.Add(this.Slot11);
+            this.Controls.Add(this.Slot10);
+            this.Controls.Add(this.Slot9);
+            this.Controls.Add(this.Slot8);
+            this.Controls.Add(this.Slot7);
+            this.Controls.Add(this.Slot6);
+            this.Controls.Add(this.Slot5);
+            this.Controls.Add(this.Slot4);
+            this.Controls.Add(this.Slot3);
+            this.Controls.Add(this.Slot2);
+            this.Controls.Add(this.Slot1);
+            this.Name = "SlotMachineForm";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.Slot1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Slot2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Slot3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Slot4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Slot5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Slot6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Slot7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Slot8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Slot9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Slot10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Slot11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Slot12)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Slot13)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Slot14)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Slot15)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Bet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Slot1;
+        private System.Windows.Forms.PictureBox Slot2;
+        private System.Windows.Forms.PictureBox Slot3;
+        private System.Windows.Forms.PictureBox Slot4;
+        private System.Windows.Forms.PictureBox Slot5;
+        private System.Windows.Forms.PictureBox Slot6;
+        private System.Windows.Forms.PictureBox Slot7;
+        private System.Windows.Forms.PictureBox Slot8;
+        private System.Windows.Forms.PictureBox Slot9;
+        private System.Windows.Forms.PictureBox Slot10;
+        private System.Windows.Forms.PictureBox Slot11;
+        private System.Windows.Forms.PictureBox Slot12;
+        private System.Windows.Forms.PictureBox Slot13;
+        private System.Windows.Forms.PictureBox Slot14;
+        private System.Windows.Forms.PictureBox Slot15;
+        private System.Windows.Forms.TextBox totalMoney;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown Bet;
+        private System.Windows.Forms.Button SpinBtn;
+        private System.Windows.Forms.Button CashOutBtn;
+        private System.Windows.Forms.Label WinLabel;
+        private System.Windows.Forms.TextBox WinTB;
+        private System.Windows.Forms.Button musicBtn;
+    }
+}
+
